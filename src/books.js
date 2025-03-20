7ea835c24d3a139615c17885484b14c3d6f7d3a3
SergioCaMi <sergiocami84@gmail.com>
2025-03-20 18:00:00 +0100

    ejercicio 2

--- src/books.js
+++ src/books.js
@@ -1,3 +1,13 @@
+
+// Iteration 1 | Books Array
+// In the index.js file, create an array named booksArray containing 4 objects representing the books described in the code snippet below. Each object should have the following properties:
+
+// title
+// pages
+// author
+// details
+
+// The values of the properties should be the same as the ones in the following code snippet:
 // Iteration 1 | Books Array
 
 // Book 1
@@ -36,30 +46,70 @@
 //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
 // }
 
-
-// Your code here:
-const booksArray = [];
-
-
-
+console.log("Ejercicio 1");
+let booksArray = [
+  {
+      title: "The Old Man and the Sea",
+      pages: 128,
+      author: "Ernest Hemingway",
+      details: {
+          language: "English",
+          description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+      }
+  },
+  {
+      title: "The Airbnb Story",
+      pages: 256,
+      author: "Leight Gallagher",
+      details: {
+          language: "English",
+          description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+      }
+  },
+  {
+      title: "Educated - A Memoir",
+      pages: 352,
+      author: "Tara Westover",
+      details: {
+          language: "English",
+          description: "Educated is an account of the struggle for self-invention..."
+      }
+  },
+  {
+      title: "The Art of Learning",
+      pages: 288,
+      author: "Josh Waitzkin",
+      details: {
+          language: "English",
+          description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+      }
+  }
+];
+console.log(booksArray);
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
+// Create a function named getBookDetails() that takes one argument - the book object. The function should return a string in the following format:
 
+"TITLE - AUTHOR - PAGES pages"
+// Iteration 2 | Book Details
+console.log("Ejercicio 2");
+function getBookDetails(book) {
+  // Your code here:
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
 }
 
+booksArray.forEach((book, index) => {console.log(getBookDetails(book));});
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
+console.log("Ejercicio 3");
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
+console.log("Ejercicio 4");
 
 
 
@@ -68,6 +118,7 @@ function getBookDetails() {
 /* The `dictionary` is an object containing books grouped by author. 
  The book info is stored in arrays with structure: [title, pages]. 
 */
+console.log("Ejercicio 5");
 const dictionary = {
     "J. K. Rowling": [
         ["Harry Potter and the Philosopher's Stone", 223],
@@ -94,6 +145,7 @@ function booksByAuthor() {
 
 
 // Bonus: Iteration 6 | Average Page Count
+console.log("Ejercicio 6");
 function averagePageCount() {
   // Your code here:
   
